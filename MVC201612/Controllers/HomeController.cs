efab60d5a301048f783da66470f3404c4ad1da1f
devcronberg <devcronberg@gmail.com>
2016-12-06 16:24:31 +0100

    .

---
+++ MVC201612/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using MVC201612.Models.Misc;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC201612.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            var controlleractionlist = asm.GetTypes()
+                    .Where(type => typeof(System.Web.Mvc.Controller).IsAssignableFrom(type))
+                    .SelectMany(type => type.GetMethods(BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public))
+                    .Where(m => !m.GetCustomAttributes(typeof(System.Runtime.CompilerServices.CompilerGeneratedAttribute), true).Any())
+                    .Select(x => new ControllerActionViewModel { ControllerName = x.DeclaringType.Name, ActionName = x.Name, ReturnType = x.ReturnType.Name, Attributes = String.Join(",", x.GetCustomAttributes().Select(a => a.GetType().Name.Replace("Attribute", ""))) })
+                    .OrderBy(x => x.ControllerName).ThenBy(x => x.ActionName).ToList();
+            var grp = controlleractionlist.GroupBy(i => i.ControllerName).ToDictionary(i => i.Key, i=>i.ToList());
+            return View(grp);
+        }
+    }
+}
---
+++ MVC201612/Models/Misc/ControllerActionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC201612.Models.Misc
+{
+    public class ControllerActionViewModel
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string ReturnType { get; set; }
+
+        public string Attributes { get; set; }
+    }
+}
