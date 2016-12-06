21980f5393b9251ba80e84da66d2c218f7ac1ed0
Michell Cronberg <devcronberg@gmail.com>
2016-12-06 13:32:14 +0100

    .

--- MVC201612/Controllers/Module03Controller.cs
+++ MVC201612/Controllers/Module03Controller.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MVC201612.Models.Module02;
+using MVC201612.Models.Module03;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -56,5 +59,66 @@ namespace MVC201612.Controllers
         {
             return View("TestView");
         }
+
+        public ActionResult View4()
+        {
+            var lst = new MVC201612.Models.Module02.PersonHelper().GetAllPeople();
+            ViewBag.fakturaer = Guid.NewGuid().ToString();
+            return View(lst);
+
+        }
+
+        public ActionResult View5()
+        {
+            var lst = new MVC201612.Models.Module02.PersonHelper().GetAllPeople();
+            ViewBag.fakturaer = Guid.NewGuid().ToString();
+            return View(lst);
+
+        }
+
+        public ActionResult View6()
+        {
+            PersonFakturaViewModel m = new PersonFakturaViewModel();
+            m.Personer = new MVC201612.Models.Module02.PersonHelper().GetAllPeople();
+            m.Faktura = Guid.NewGuid().ToString();
+            return View(m);
+
+        }
+
+        public ActionResult View7()
+        {
+            //var t = new { name = "Mikkel" };
+            dynamic t = new ExpandoObject();
+            t.name = "Mikkel";
+            return View(t);
+
+        }
+
+        public ActionResult GetUser(string Username, int? age)
+        {
+            return View();
+
+        }
+
+        public ActionResult GetUser2()
+        {
+
+            return View(new User() { Username = "ælkælk" });
+
+        }
+        [HttpPost]
+        public ActionResult GetUser2(User u)
+        {
+            return View();
+
+        }
+
+    }
+
+    // Skal ikke ligge her med mangler kage.. (skal ligge i /models)
+    class PersonFakturaViewModel {  // Hedder også DTO'er PersonFakturaDTO
+        public List<Person> Personer { get; set; }
+        public string Faktura { get; set; }
+        // + diverse
     }
 }
---
+++ MVC201612/Models/Module03/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC201612.Models.Module03
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public int Age { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
